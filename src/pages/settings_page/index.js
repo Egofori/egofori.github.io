43232bbe9af8533fdc52839c0f72c7dd05cb7e84
Egofori <evansgyan7@gmail.com>
2020-07-01 08:42:16 +0000

    Updates

--- src/pages/settings_page/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import MainComponent from "./components/MainComponent";
-
-let root = document.getElementById("root");
-ReactDOM.render(<MainComponent />, root);
